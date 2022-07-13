ffe69387bf0c4f6ca5035a12928411e4b78cbacf
Pichau <gustavosandrindattein123@gmail.com>
2022-07-13 11:34:42 -0300

    alterado o settings

--- portifolio/settings.py
+++ portifolio/settings.py
@@ -121,10 +121,10 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'mediafiles')
 
 COLLECTFAST_ENABLED = False
 
-AWS_ACCESS_KEY_ID = config('AWS_ACESS_KEY_ID', default=False)
+AWS_ACCESS_KEY_ID = config('AWS_ACCESS_KEY_ID')
 
-# STORAGE CONFIGURATION IN S3 AWS
-# -------------------------------------------------------
+# storage configuration in s3 AWS
+# ---------------------------------------
 
 if AWS_ACCESS_KEY_ID:
     AWS_SECRET_ACCESS_KEY = config('AWS_SECRET_ACCESS_KEY')
@@ -139,17 +139,14 @@ if AWS_ACCESS_KEY_ID:
     COLLECTFAST_ENABLED = True
 
     AWS_DEFAULT_ACL = 'private'
-
-    # Static Assets
-    # ---------------------------------------------------
+    # static assets
     STATICFILES_STORAGE = 's3_folder_storage.s3.StaticStorage'
     STATIC_S3_PATH = 'static'
     STATIC_ROOT = f'/{STATIC_S3_PATH}/'
     STATIC_URL = f'//s3.amazonaws.com/{AWS_STORAGE_BUCKET_NAME}/{STATIC_S3_PATH}/'
     ADMIN_MEDIA_PREFIX = STATIC_URL + 'admin/'
 
-    # Upload Media Assets
-    # ---------------------------------------------------
+    # Upload Media Folder
     DEFAULT_FILE_STORAGE = 's3_folder_storage.s3.DefaultStorage'
     DEFAULT_S3_PATH = 'media'
     MEDIA_ROOT = f'/{DEFAULT_S3_PATH}/'
