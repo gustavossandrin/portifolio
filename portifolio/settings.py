444221dbe523f5be3b46ccac54b4c5ef54d6180c
Pichau <gustavosandrindattein123@gmail.com>
2022-07-12 23:21:09 -0300

    instalada lib python-decouple

--- portifolio/settings.py
+++ portifolio/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
+from decouple import config
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
@@ -23,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-1(vq5zyq#^6(f65gkx4(x%)^k6ih0v-1qy5mrb(eui24pm%28+'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', cast=bool)
 
 ALLOWED_HOSTS = ['*']
 
